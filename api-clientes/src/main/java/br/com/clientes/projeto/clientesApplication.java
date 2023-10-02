b60c5d50551bce9066209438c4b91cd90d9c03ae
marcus aurelio <marcusaurelio743@gmail.com>
2023-10-02 15:14:14 -0300

    criando api de clientes

---
+++ api-clientes/src/main/java/br/com/clientes/projeto/clientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.clientes.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class clientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(clientesApplication.class, args);
+	}
+
+}
