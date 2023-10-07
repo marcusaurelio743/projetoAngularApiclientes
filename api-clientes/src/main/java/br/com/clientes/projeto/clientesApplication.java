bd8c5c5d54c77b7e1cfc7e8a0e6ad356f8624672
marcus aurelio <marcusaurelio743@gmail.com>
2023-10-07 12:21:17 -0300

    atualizando projeto

--- api-clientes/src/main/java/br/com/clientes/projeto/clientesApplication.java
+++ api-clientes/src/main/java/br/com/clientes/projeto/clientesApplication.java
@@ -1,13 +1,7 @@
 package br.com.clientes.projeto;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import br.com.clientes.projeto.model.Cliente;
-import br.com.clientes.projeto.repository.ClienteRepository;
 
 @SpringBootApplication
 public class clientesApplication {
--- api-clientes/src/main/java/br/com/clientes/projeto/rest/ClienteController.java
+++ api-clientes/src/main/java/br/com/clientes/projeto/rest/ClienteController.java
@@ -1,12 +1,19 @@
 package br.com.clientes.projeto.rest;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.clientes.projeto.model.Cliente;
 import br.com.clientes.projeto.repository.ClienteRepository;
@@ -20,8 +27,44 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar( @RequestBody Cliente cliente) {
+	public Cliente salvar( @RequestBody @Valid Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	
+	@GetMapping("{id}")
+	public Cliente buscaPorId(@PathVariable Long id) {
+		return repository.
+				findById(id).
+				//expresão ororElseThrow e se caso não achar retorna esse erro
+				orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void Deletar(@PathVariable Long id) {
+		 repository.
+				findById(id).
+				map(cliente->{
+					repository.delete(cliente);
+					return Void.TYPE;
+				})
+				.orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizar(@PathVariable Long id,@RequestBody @Valid Cliente clienteAtualizado) {
+		repository.
+				findById(id).
+					map(cliente->{
+						clienteAtualizado.setId(cliente.getId());
+						clienteAtualizado.setDataCadastro(cliente.getDataCadastro());
+						
+						return repository.save(clienteAtualizado);
+					})
+					.orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+		
 	}
 
 }
